9e06a8d3d24ee5aeebc6c63cd6fe360563d00cdf
Kononov <kononovv@9801a7a518f9.ant.amazon.com>
2017-03-16 20:36:29 +0000

    add initial js files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    //player.handleInput(allowedKeys[e.keyCode]);
+});
---
+++ js/engine.js
@@ -0,0 +1,59 @@
+
+var Engine = (function(global) {
+
+    var doc = global.document,
+        win = global.window,
+        canvas = doc.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        lastTime;
+
+    canvas.width = 505;
+    canvas.height = 606;
+    doc.body.appendChild(canvas);
+
+    /* This function serves as the kickoff point for the game loop itself
+     * and handles properly calling the update and render methods.
+     */
+    function main() {
+
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
+
+        update(dt);
+        render();
+
+        lastTime = now;
+
+        win.requestAnimationFrame(main);
+    }
+
+    function init() {
+        reset();
+        lastTime = Date.now();
+        main();
+    }
+
+    function update(dt) {
+    }
+
+    function render() {
+        /*for (row = 0; row < numRows; row++) {
+            for (col = 0; col < numCols; col++) {
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+            }
+        }*/
+    }
+
+    function reset() {
+    }
+
+    Resources.load([
+    ]);
+    Resources.onReady(init);
+
+    /* Assign the canvas' context object to the global variable (the window
+     * object when run in a browser) so that developers can use it more easily
+     * from within their app.js files.
+     */
+    global.ctx = ctx;
+})(this);
