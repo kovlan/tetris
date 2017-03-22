6351c19e72580b2f35c949ff5bb3fadfde36e973
Kononov <kononovv@9801a7a518f9.ant.amazon.com>
2017-03-22 00:01:33 +0000

    finish engine poc

--- js/app.js
+++ js/app.js
@@ -1,10 +1,313 @@
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+//====================================
+// ImageSet - all images, indexes, file paths
 
-    //player.handleInput(allowedKeys[e.keyCode]);
-});
+var ImageSet = function() {
+    this.folder = 'images';
+    this.ext = '.png';
+    this.id2name = ['bkg', 'red', 'green', 'blue', 'yellow', 'purple', 'brown', 'pink'];
+}
+
+ImageSet.prototype.getImageByIndex = function(index) {
+    return this.id2name[index];
+}
+
+ImageSet.prototype.getPathByIndex = function(index) {
+    return this.folder + '/' + this.id2name[index] + this.ext;
+}
+
+ImageSet.prototype.getBkgPath = function() {
+    return this.getPathByIndex(0);
+}
+
+ImageSet.prototype.getAllPaths = function() {
+    var result = [];
+    for (var i in this.id2name) {
+        result.push(this.getPathByIndex(i));
+    }
+    return result;
+}
+
+
+//===================================
+// Field - current status of game field
+
+var Field = function(cols, rows) {
+    console.assert(rows > 0 && cols > 0);
+    console.assert(rows < 1000 && cols < 1000);
+
+    this.rows = rows;
+    this.cols = cols;
+    this.elems = new Array(rows);
+    for (var i = 0; i < rows; ++i) {
+        this.elems[i] = new Array(cols);
+        for (var j = 0; j < cols; ++j) {
+            this.elems[i][j] = 0;
+        }
+    }
+}
+
+Field.prototype.getValue = function(x, y) {
+    return this.elems[y][x];
+}
+
+Field.prototype.isValidElem = function(elem) {
+    var pts = elem.getPts();
+    for (var i in pts) {
+        var x = Math.floor(pts[i].x);
+        if (x < 0 || x >= this.cols || pts[i].y >= this.rows - 1) {
+            return false;
+        }
+
+        var y = Math.floor(pts[i].y);
+        if (y >= 0 && this.elems[y][x] > 0) {
+            return false;
+        }
+
+        y = Math.ceil(pts[i].y);
+        if (y >= 0 && this.elems[y][x] > 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+Field.prototype.freezeElem = function(elem) {
+    var pts = elem.getPts();
+    for (var i in pts) {
+        var x = Math.floor(pts[i].x);
+        var y = Math.floor(pts[i].y);
+        if (x >= 0 && x < this.cols &&
+            y >= 0 && y < this.rows) {
+            this.elems[y][x] = elem.getID();
+        }
+    }
+}
+
+Field.prototype.checkAndDelete = function() {
+    for (var y = this.rows-1; y >= 0; --y) {
+        var all = true;
+        for (var x = 0; x < this.cols; ++x) {
+            if (this.elems[y][x] == 0) {
+                all = false;
+            }
+        }
+        if (!all) {
+            continue;
+        }
+        for (var y2 = y; y2 >= 1; --y2) {
+            for (var x2 = 0; x2 < this.cols; ++x2) {
+                this.elems[y2][x2] = this.elems[y2-1][x2];
+            }
+        }
+        for (var x2 = 0; x2 < this.cols; ++x2) {
+            this.elems[0][x2] = 0;
+        }
+    }
+}
+
+
+//======================================
+// Tetelem - tetris element
+var Tetelem = function(points, speed, id) {
+    this.pts = points;
+    this.speed = speed;
+    this.id = id;
+
+    this.center = { 'x': 0, 'y': 0};
+    for (var i = 0; i < this.pts.length; ++i) {
+        this.center.x += this.pts[i].x;
+        this.center.y += this.pts[i].y;
+    }
+    this.center.x = Math.round(this.center.x / this.pts.length);
+    this.center.y = Math.round(this.center.y / this.pts.length);
+    this.computeCenter();
+}
+
+Tetelem.prototype.computeCenter = function() {
+    this.center = { 'x': 0, 'y': 0};
+    for (var i = 0; i < this.pts.length; ++i) {
+        this.center.x += this.pts[i].x;
+        this.center.y += this.pts[i].y;
+    }
+    this.center.x = Math.round(this.center.x / this.pts.length);
+    this.center.y = Math.round(this.center.y / this.pts.length);
+}
+
+Tetelem.prototype.update = function(dt) {
+    for (var i in this.pts) {
+        this.pts[i].y += this.speed * dt;
+    }
+    this.center.y += this.speed * dt;
+}
+
+Tetelem.prototype.getID = function() {
+    return this.id;
+}
+
+Tetelem.prototype.getPts = function() {
+    return this.pts;
+}
+
+Tetelem.prototype.setPts = function(pts) {
+    this.pts = pts;
+    this.computeCenter();
+}
+
+Tetelem.prototype.rotateCW = function() {
+    var rotatePoint = function(pt, center) {
+        var xv = pt.x - center.x;
+        var yv = pt.y - center.y;
+        return { 'x': Math.round(center.x - yv), 'y': Math.round(center.y + xv)};
+    }
+
+    var new_pts = [];
+    for (i in this.pts) {
+        new_pts.push(rotatePoint(this.pts[i], this.center));
+    }
+    this.pts = new_pts;
+    this.computeCenter();
+}
+
+Tetelem.prototype.rotateCCW = function() {
+    var rotatePoint = function(pt, center) {
+        var xv = pt.x - center.x;
+        var yv = pt.y - center.y;
+        return { 'x': Math.round(center.x + yv), 'y': Math.round(center.y - xv)};
+    }
+
+    var new_pts = [];
+    for (i in this.pts) {
+        new_pts.push(rotatePoint(this.pts[i], this.center));
+    }
+    this.pts = new_pts;
+    this.computeCenter();
+}
+
+Tetelem.prototype.moveLeft = function() {
+    for (i in this.pts) {
+        this.pts[i].x--;
+    }
+    this.center.x--;
+}
+
+Tetelem.prototype.moveRight = function() {
+    for (i in this.pts) {
+        this.pts[i].x++;
+    }
+    this.center.x++;
+}
+
+Tetelem.prototype.moveDown = function() {
+    for (i in this.pts) {
+        this.pts[i].y++;
+    }
+    this.center.y++;
+}
+
+Tetelem.prototype.moveUp = function() {
+    for (i in this.pts) {
+        this.pts[i].y--;
+    }
+    this.center.y--;
+}
+
+Tetelem.prototype.round = function() {
+    for (i in this.pts) {
+        this.pts[i].y = Math.round(this.pts[i].y);
+    }
+    this.center.y = Math.round(this.center.y);
+}
+
+Tetelem.prototype.floor = function() {
+    for (i in this.pts) {
+        this.pts[i].y = Math.floor(this.pts[i].y);
+    }
+    this.center.y = Math.floor(this.center.y);
+}
+
+
+//====================================
+// Factory for tetris elements
+var base_speed = 2;
+var dspeed = 0.05;
+var createTetelem = function(width, height) {
+    id = Math.floor((Math.random() * 7) + 1);
+    x = Math.floor((Math.random() * (width - 6)) + 3);
+    y = 0;
+    pts = [];
+    switch (id) {
+        case 1:  // ----
+            pts.push({'x': x, 'y': y});
+            pts.push({'x': x-1, 'y': y});
+            pts.push({'x': x+1, 'y': y});
+            pts.push({'x': x-2, 'y': y});
+            break;
+        case 2:  // _|^
+            pts.push({'x': x, 'y': y});
+            pts.push({'x': x-1, 'y': y});
+            pts.push({'x': x, 'y': y+1});
+            pts.push({'x': x+1, 'y': y+1});
+            break;
+        case 3:  // ^|_
+            pts.push({'x': x, 'y': y});
+            pts.push({'x': x+1, 'y': y});
+            pts.push({'x': x, 'y': y+1});
+            pts.push({'x': x-1, 'y': y+1});
+            break;
+        case 4:  // ___|
+            pts.push({'x': x, 'y': y});
+            pts.push({'x': x-1, 'y': y});
+            pts.push({'x': x+1, 'y': y});
+            pts.push({'x': x+1, 'y': y-1});
+            break;
+        case 5:  // ^T^
+            pts.push({'x': x, 'y': y});
+            pts.push({'x': x-1, 'y': y});
+            pts.push({'x': x+1, 'y': y});
+            pts.push({'x': x, 'y': y+1});
+            break;
+        case 6:  // ^^|
+            pts.push({'x': x, 'y': y});
+            pts.push({'x': x-1, 'y': y});
+            pts.push({'x': x+1, 'y': y});
+            pts.push({'x': x+1, 'y': y+1});
+            break;
+        case 7:  // ||
+            pts.push({'x': x, 'y': y});
+            pts.push({'x': x-1, 'y': y});
+            pts.push({'x': x, 'y': y+1});
+            pts.push({'x': x-1, 'y': y+1});
+            break;
+        default:
+            console.assert(false);
+    }
+
+    var rots = Math.floor((Math.random() * 4));
+    var result = new Tetelem(pts, base_speed, id);
+    for (var i = 0; i < rots; ++i) {
+        result.rotateCW();
+    }
+    base_speed += dspeed;
+    return result;
+}
+
+
+//============================================
+// control
+
+var Actions = function() {
+    this.seq = [];
+}
+
+Actions.prototype.add = function(a) {
+    this.seq.push(a);
+}
+
+Actions.prototype.get = function() {
+    return this.seq;
+}
+
+Actions.prototype.clear = function() {
+    this.seq = [];
+}
+
--- js/engine.js
+++ js/engine.js
@@ -7,14 +7,38 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    var canvas_width = 320;
+    var canvas_height = 520;
+    var offset_x = 10;
+    var offset_y = 10;
+    var elem_width = 20;
+    var elem_height = 20;
+    var field_width = 15;
+    var field_height = 25;
+    var field = new Field(field_width, field_height);
+    var image_set = new ImageSet();
+    var isElemInField = false;
+    var elem = {};
+    var count = 1000;
+    ctx.actions = new Actions();
+
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
     function main() {
+
+        if (!isElemInField) {
+            elem = createTetelem(field_width, field_height);
+            if (!field.isValidElem(elem)) {
+                return;
+                field = new Field(field_width, field_height);
+            }
+            isElemInField = true;
+        }
 
         var now = Date.now(),
             dt = (now - lastTime) / 1000.0;
@@ -24,7 +48,10 @@ var Engine = (function(global) {
 
         lastTime = now;
 
-        win.requestAnimationFrame(main);
+        //count--;
+        //if (count > 0) {
+            win.requestAnimationFrame(main);
+        //}
     }
 
     function init() {
@@ -33,22 +60,89 @@ var Engine = (function(global) {
         main();
     }
 
+    function act() {
+        acts = ctx.actions.get();
+        for (a in acts) {
+            var backup_pts = [];
+            var pts = elem.getPts();
+            for (i in pts) {
+                backup_pts.push({'x': pts[i].x, 'y': pts[i].y});
+            }
+            var full_break = false;
+
+            switch (acts[a]) {
+            case 'left':
+                elem.moveLeft();
+                break;
+            case 'right':
+                elem.moveRight();
+                break;
+            case 'up':
+                elem.rotateCCW();
+                break;
+            case 'down':
+                elem.rotateCW();
+                break;
+            case 'space':
+                while (field.isValidElem(elem)) {
+                    elem.moveDown();
+                }
+                elem.floor();
+                full_break = true;
+                break;
+            }
+
+            if (full_break) {
+                break;
+            }
+            if (!field.isValidElem(elem)) {
+                elem.setPts(backup_pts);
+            }
+        }
+        ctx.actions.clear();
+    }
+
     function update(dt) {
+        act();
+        
+        elem.update(dt);
+        if (!field.isValidElem(elem)) {
+            field.freezeElem(elem);
+            isElemInField = false;
+            field.checkAndDelete();
+        }
     }
 
     function render() {
-        /*for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+        ctx.drawImage(Resources.get(image_set.getBkgPath()), 0, 0);
+        for (row = 0; row < field_height; row++) {
+            for (col = 0; col < field_width; col++) {
+                var ind = field.getValue(col, row);
+                if (ind > 0) {
+                    ctx.drawImage(Resources.get(image_set.getPathByIndex(ind)),
+                        offset_x + col * elem_width,
+                        offset_y + row * elem_height);
+                }
             }
-        }*/
+        }
+
+        if (!isElemInField) {
+            return;
+        }
+
+        elem_img = Resources.get(image_set.getPathByIndex(elem.getID()));
+        elem_pts = elem.getPts();
+        for (var i in elem_pts) {
+            ctx.drawImage(elem_img,
+                          offset_x + elem_pts[i].x * elem_width,
+                          offset_y + elem_pts[i].y * elem_height);
+        }
     }
 
     function reset() {
     }
 
-    Resources.load([
-    ]);
+    Resources.load(image_set.getAllPaths());
     Resources.onReady(init);
 
     /* Assign the canvas' context object to the global variable (the window
@@ -56,4 +150,19 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
+
+    document.addEventListener('keyup', function(e) {
+        var allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down',
+            32: 'space'
+        };
+
+        if (allowedKeys[e.keyCode]) {
+            ctx.actions.add(allowedKeys[e.keyCode]);
+        }
+    });
+
 })(this);
